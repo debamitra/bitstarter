fa315f983b14d1f68e633c42d619f2231745dd2b
debamitra.mukherjee@gmail.com <ubuntu@ip-172-31-35-227.us-west-2.compute.internal>
2013-07-10 22:05:01 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
+var fs = require('fs');
+fs.readFileSync('bitstarter/index.html', function (err, data) {
+  if (err) throw err;
+  data.toString();
+});
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send("howdy");
 });
 
 var port = process.env.PORT || 5000;
